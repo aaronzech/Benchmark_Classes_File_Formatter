094aea01f882ed63498630647593ed8bb1b4d538
Aaron Zech <aaronzech@gmail.com>
2022-10-27 14:50:59 -0500

    Removed commented out lines

--- benchmark_classes_file.py
+++ benchmark_classes_file.py
@@ -2,26 +2,14 @@ import openpyxl
 import pandas as pd
 from openpyxl import Workbook,load_workbook
 from openpyxl.utils import get_column_letter
-
 import tkinter as tk
 from tkinter import filedialog
+
 root = tk.Tk()
 root.withdraw()
 print("Select class file")
 class_file = filedialog.askopenfilename()
 teacher_file = 'Classes.xlsx'
-
-#print(ws)
-#print(ws['A1'].value) # Read value at A1
-
-#ws['M2'].value = "TEST" # Change value at A2
-#wb.save('Classes.xlsx')
-
-# for row in range (1,900): #stops at row 10
-#     for col in range (1,7): #columns 1 -4
-#         char = get_column_letter(col)
-#         print(ws[char + str(row)].value)
-
 
 # Change the Kindergarten Grade code from KF or 25 to K.
 def fixKindergartenGradeLevel(file):
@@ -39,8 +27,7 @@ def fixKindergartenGradeLevel(file):
 def formatSheet(file):
     wb = load_workbook(file) #Load Workbook
     ws = wb.active #Worksheet
-    #print(ws)
-    
+
     #Concat Class Name with Last Name
     for row in range (1,900): #stops at row 10
      for col in range (3,4): #columns 1 -4
@@ -69,13 +56,6 @@ def formatSheet(file):
 
 # Attach the teachers to the end of the class file.
 def pastInTeachers(file,file2):
-    wb = load_workbook(file) #Load Workbook
-    ws = wb.active #Worksheet
-    last_empty_row = len(list(ws.rows))
-    print(last_empty_row) #809 migh tneed to plus one this
-    # Append or just hard code it in. 
-    #Concat to data frames that share common columns
-    # load in the various tables from an excel document
     file = pd.read_excel(file,sheet_name='QRY801')
     file2 = pd.read_excel('Classes.xlsx',sheet_name='Teachers')
     merge = pd.concat([file,file2],axis=0) 
@@ -97,8 +77,3 @@ if __name__ == "__main__":
     pastInTeachers(class_file,teacher_file)
     deleteExtraColumns('classes_appened.xlsx')
     print("done")
-    #mergeSheets(student_file,clever_file)
-    #formatSheet('merge.xlsx')
-    #cleverData = pd.read_excel('merge.xlsx',sheet_name='Sheet1')
-    #cleverData.to_csv("students_IMPORT.csv",index=False)
-    #print("finished")
